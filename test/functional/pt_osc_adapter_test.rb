089f1c87c1b0b510bae49e3e65a18735bc561a5a
Steve Rice <steve@pagerduty.com>
2014-06-06 11:35:30 -0700

    refactor test_spec into test_helper

--- test/functional/pt_osc_adapter_test.rb
+++ test/functional/pt_osc_adapter_test.rb
@@ -6,10 +6,10 @@ class PtOscAdapterTest < Test::Unit::TestCase
 
   def test_connection
     # Test that we can open a connection with the pt_osc adapter
-    test_spec = YAML.load_file(Rails.root.join(*%w(.. config database.yml)))['test']
-    test_spec['adapter'] = 'pt_osc'
+    spec = test_spec
+    spec.delete('database') # We don't care whether the database exists
 
-    assert_nothing_raised { TestConnection.establish_connection(test_spec) }
+    assert_nothing_raised { TestConnection.establish_connection(spec) }
     assert_equal true, TestConnection.connection.in_use
     assert_kind_of ActiveRecord::ConnectionAdapters::PtOscAdapter, TestConnection.connection
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,9 @@ require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'test/unit'
 
 Rails.backtrace_cleaner.remove_silencers!
+
+def test_spec
+  test_spec = YAML.load_file(Rails.root.join(*%w(.. config database.yml)))['test']
+  test_spec['adapter'] = 'pt_osc'
+  test_spec
+end
