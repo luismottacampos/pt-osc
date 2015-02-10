4b355acfc2c831c1da2d651ef3f3593df4a41b10
Steve Rice <steve@steverice.org>
2015-02-09 21:51:46 -0800

    Test additional versions of percona toolkit
    Since they don't appear to be in an apt repository anywhere, download the deb files directly from percona.

--- test/travisci/install_pt_osc.sh
+++ test/travisci/install_pt_osc.sh
@@ -16,6 +16,15 @@ sudo apt-get update -qq
 # Install the right version
 case $PT_OSC_VERSION in
   LATEST|DEVELOPMENT) sudo apt-get install -y percona-toolkit; ;;
-  *) sudo apt-get install -y percona-toolkit=$PT_OSC_VERSION; ;;
+  2.2.*)
+    wget http://www.percona.com/downloads/percona-toolkit/"$PT_OSC_VERSION"/deb/percona-toolkit_"$PT_OSC_VERSION"_all.deb
+    sudo dpkg -i percona-toolkit_"$PT_OSC_VERSION"_all.deb
+    sudo apt-get install -f -y
+    ;;
+  *)
+    wget http://www.percona.com/downloads/percona-toolkit/"$PT_OSC_VERSION"/percona-toolkit_"$PT_OSC_VERSION"_all.deb
+    sudo dpkg -i percona-toolkit_"$PT_OSC_VERSION"_all.deb
+    sudo apt-get install -f -y
+    ;;
 esac
 
